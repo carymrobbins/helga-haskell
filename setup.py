763020530703a3e443cecfd2fac1ce5f49c34b43
Cary M. Robbins <carymrobbins@gmail.com>
2014-06-26 20:35:39 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='helga-haskell',
-    use_hg_version=True,
+    version='0.1.0',
     author="Cary M. Robbins",
     author_email="carymrobbins@gmail.com",
-    url="",
+    url="https://github.com/carymrobbins/helga-haskell",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=True,
     install_requires=[
-        # 'helga',
-        # 'tryhaskell',
+        'helga',
+        'tryhaskell',
     ],
     entry_points=dict(
         helga_plugins=[
